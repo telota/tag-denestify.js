42c9a257d3452412dfb33d8090f33cbf3061db6a
lphilipp <lphilipp@bbaw.de>
2017-10-05 07:49:21 +0200

    fix problem with empty tag-pairs

--- tag-formatter.js
+++ tag-formatter.js
@@ -3,108 +3,64 @@
  * @param string string with nested tags
  * @returns {string} string with unnested tags
  */
-formatTagString = (string) =>{
-    let completeTagArray = buildNewTagArray(splitStringByTag(string));
-    let contentArray = splitStringIntoContent(string);
+formatTagString = (string) => {
+    let reg = new RegExp("(<.*?>)", "g");
+    let contentArray = string.split(reg);
 
-    //counting variables
-    let index;
-    let tagIndex = 0;
-
-    if(string.startsWith(contentArray[0],0)) {index = 1;}
-    else {index = 0;}
-
-    for(index; index < contentArray.length; index = index+3){
-        contentArray.splice(index, 0, completeTagArray[tagIndex]);
-        contentArray.splice(index+2, 0, completeTagArray[tagIndex+1]);
-
-        tagIndex = tagIndex+2;
-    }
-    return contentArray.join("");
-
-}
-
-/**
- * splits String into an Array containing all Tags
- * @param string string with tags and content
- * @returns {Array.<T>} Array with extracted Tags
- */
-splitStringByTag = (string) =>{
-    let reg = new RegExp("<.*?>", "g");
-    return string.match(reg);
-}
-
-/**
- * splits String into Array containing all non-empty ("") content
- * @param string with tags and content
- * @returns {Array.<T>} Array with extracted Content
- */
-splitStringIntoContent = (string) =>{
-    let reg = new RegExp("<.*?>", "g");
-    let contentArray =string.split(reg);
-
-    let filtered = contentArray.filter(entry => {
-        return entry.length > 0;
-    })
-    return filtered;
-}
-
-/**
- *checks if Tags in Array are in correct order and complete pairs.
- *Completes incomplete tag-pairs
- * @param tagArray Array containing tags
- * @returns {Array.<T>} correctly ordered tag-array
- */
-buildNewTagArray = (tagArray) => {
     let tagStringClosed = "";
     let tagStringOpening = "";
 
-    //Array containing all opening Strings which fit into <t> </t> scheme
-    let wholeTagStringArray =[];
+    //Array for opened tags without corresponding closing tags
+    let wholeTagStringArray = [];
 
-    //check tagArray in pairs if every Tag has a matching partner (<t> </t> <d> </d>...)
-    for (let i = 0; i < tagArray.length; i = i + 2) {
+    for (let index = 0; index < contentArray.length; index++) {
 
-        tagStringClosed = "</" + getOpeningTagName (tagArray[i])+ ">";
-        tagStringOpening = "<" + getClosingTagName(tagArray[i])+ ">";
+        //If element is an opening tag
+        if ((/<[^\/].*>/.test(contentArray[index]))) {
+            tagStringClosed = "</" + getOpeningTagName(contentArray[index]) + ">";
+            tagStringOpening = "<" + getClosingTagName(contentArray[index]) + ">";
 
-        //if tagArray[i] is a closing tag, the new tag will be inserted before it
-        if((/<\/.*>/.test(tagArray[i]))){
-            for(let j = 0; j < wholeTagStringArray.length; j++){
-
-                if (getOpeningTagName(wholeTagStringArray[j]) === getClosingTagName(tagArray[i])){
-                    tagArray.splice(i,0,wholeTagStringArray[j]);
-                }
+            //Array-structure looking like ["<t>", "Hello", "</t>"] - every second entry is a tag
+            if (contentArray[index + 2] !== tagStringClosed) {
+                contentArray.splice(index + 2, 0, tagStringClosed);
+                wholeTagStringArray.push(contentArray[index]);
+                index = index + 2;
             }
         }
 
-        //if tagArray[i] is an opening tag, the new tag will be inserted after it
-        else if ((tagStringClosed != tagArray[i + 1])) {
-             tagArray.splice(i+1,0,tagStringClosed);
-             wholeTagStringArray.push(tagArray[i]);
+        else if ((/<\/.*>/.test(contentArray[index]))) {
+
+            for (let j = 0; j < wholeTagStringArray.length; j++) {
+
+                if (getOpeningTagName(wholeTagStringArray[j]) === getClosingTagName(contentArray[index])) {
+                    contentArray.splice(index - 1, 0, wholeTagStringArray[j]);
+                    wholeTagStringArray.splice(j, 1);
+                    index = index + 2;
+                }
+            }
         }
     }
-    return tagArray;
-}
+
+    return contentArray.join("").replace(/<[^<|>]*><\/[^<|>]*>/g, "");
+};
 
 /**
  * get TagName from opening-tag without tag-attributes
  * @param tagElement tag with eventual tag-attributes
- * @returns string tagname
+ * @returns string TagName
  */
-getOpeningTagName = (tagElement) =>{
+getOpeningTagName = (tagElement) => {
     //if tagArray[i] is an opening tag
     let regOpening = new RegExp("<|>");
     return tagElement.split(regOpening)[1].split(" ")[0];
-}
-
+};
 
 /**
  * get TagName from closing-tag
  * @param tagElement
- * @returns string Tagname
+ * @returns string TagName
  */
-getClosingTagName = (tagElement) =>{
+getClosingTagName = (tagElement) => {
     //if tagArray[i] is a closing tag
     let regClosing = new RegExp("<\\/|>")
 
@@ -112,13 +68,6 @@ getClosingTagName = (tagElement) =>{
 
 }
 
-
 module.exports = {
-    splitStringByTag: splitStringByTag,
-    formatTagString: formatTagString,
-    buildNewTagArray: buildNewTagArray,
-    splitStringIntoContent: splitStringIntoContent
-
-}
-
-
+    formatTagString: formatTagString
+};
--- test/tag-formatter-test.js
+++ test/tag-formatter-test.js
@@ -1,63 +1,31 @@
 import test from 'ava';
-import tagFormatter from '../tag-formatter';
-
-test('Splits String into TagArray', t => {
-
-    let formattedString = tagFormatter.splitStringByTag("<t>Hallo</t><p>Du</p>");
-    let formattedString2 = tagFormatter.splitStringByTag("<t>Hallo<p>Du</p></t>");
-
-    t.deepEqual(formattedString,["<t>", "</t>", "<p>", "</p>"] );
-    t.deepEqual(formattedString2,["<t>", "<p>", "</p>", "</t>"] );
-
-});
-
-test('Splits String into ContentArray', t => {
-
-    let formattedString = tagFormatter.splitStringIntoContent("<t>Hallo</t><p>Du</p>");
-
-    t.deepEqual(formattedString,["Hallo", "Du"] );
-
-});
-
-test('checks if Array gets rebuild correctly', t => {
-
-    let formattedString = tagFormatter.splitStringByTag("<t>Hallo<p>Du</p></t>");
-    let formattedString2 = tagFormatter.splitStringByTag('<hi rend="4_-_multiple_modification" class="modified">H<del class="deleted">ell</del>o</hi><hi rend="4_-_multiple_modification" class="modified">Su<del class="deleted">mmern</del>ote</hi>');
-
-    t.deepEqual(tagFormatter.buildNewTagArray(formattedString),["<t>", "</t>", "<p>", "</p>", "<t>", "</t>"]);
-    t.deepEqual(tagFormatter.buildNewTagArray(formattedString2),
-        ['<hi rend="4_-_multiple_modification" class="modified">', '</hi>',
-            '<del class="deleted">', '</del>',
-            '<hi rend="4_-_multiple_modification" class="modified">', '</hi>',
-            '<hi rend="4_-_multiple_modification" class="modified">', '</hi>',
-            '<del class="deleted">', '</del>',
-            '<hi rend="4_-_multiple_modification" class="modified">', '</hi>']);
-
-});
+import {formatTagString} from '../tag-formatter';
 
 test("converts String into correct new String", t =>{
 
-    t.is(tagFormatter.formatTagString("Du <t>Hallo <p>Du</p></t>"), "Du <t>Hallo </t><p>Du</p>");
-    t.is(tagFormatter.formatTagString("<t>Hallo <p>Du</p></t>"), "<t>Hallo </t><p>Du</p>");
-    t.is(tagFormatter.formatTagString('<hi rend="4_-_multiple_modification" class="modified">' +
-                        'H<del class="deleted">ell</del>o</hi>' +
-                        '<hi rend="4_-_multiple_modification" class="modified">Su<del class="deleted">' +
-                        'mmern</del>ote</hi> Bist'),
+    t.is(formatTagString("My <t>Hello <p>World</p></t>"), "My <t>Hello </t><p>World</p>");
+    t.is(formatTagString("<t>Hello <p>World</p></t>"), "<t>Hello </t><p>World</p>");
+    t.is(formatTagString('<hi rend="4_-_multiple_modification" class="modified">' +
+        'H<del class="deleted">ell</del>o</hi>' +
+        '<hi rend="4_-_multiple_modification" class="modified">Su<del class="deleted">' +
+        'mmern</del>ote</hi> Test'),
         '<hi rend="4_-_multiple_modification" class="modified">H</hi>' +
         '<del class="deleted">ell</del>' +
         '<hi rend="4_-_multiple_modification" class="modified">o</hi>' +
         '<hi rend="4_-_multiple_modification" class="modified">Su</hi>' +
         '<del class="deleted">mmern</del>' +
-        '<hi rend="4_-_multiple_modification" class="modified">ote</hi> Bist'
-        )
-
+        '<hi rend="4_-_multiple_modification" class="modified">ote</hi> Test'
+    );
+    t.is(formatTagString('<t><d>Hello</d>World</t>'),'<d>Hello</d><t>World</t>');
 });
 
 test("Testing multiple nesting", t =>{
 
-    t.is(tagFormatter.formatTagString("Du <t>Hallo <p>D<d>u</d></p></t>"),
-        "Du <t>Hallo </t><p>D</p><d>u</d>");
-    t.is(tagFormatter.formatTagString("Du <t>Hallo <p>D<d>uf<a>ff</a>hj</d></p></t>"),
-        "Du <t>Hallo </t><p>D</p><d>uf</d><a>ff</a><d>hj</d>");
+    t.is(formatTagString("My <t>Hello <p>Wor<d>ld</d></p></t>"), "My <t>Hello </t><p>Wor</p><d>ld</d>");
+    t.is(formatTagString("My <t>Hello <p>W<d>o<a>r</a>ld</d></p></t>"), "My <t>Hello </t><p>W</p><d>o</d><a>r</a><d>ld</d>");
+});
 
+test("Testing with empty Tags", t=>{
+    t.is(formatTagString("<t></t><t><d>Hello</d>World</t>"), "<d>Hello</d><t>World</t>");
+    t.is(formatTagString("<t></t><t><d>Hello</d></t><a></a>"), "<d>Hello</d>");
 });
